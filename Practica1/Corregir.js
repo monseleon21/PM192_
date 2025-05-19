59ee1f561f54c37754e574321e50f69e8dfca7c5
monseleon21 <121038307@upq.edu.mx>
2025-05-19 15:58:56 -0600

    A)
    Cmabiar var por let y const segun el codigo

---
+++ Practica1/Corregir.js
@@ -0,0 +1,9 @@
+let nombre = "Armando";
+let edad = 25;
+
+nombre = "Ana Maria";
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+console.log(saludo);
+
+// usar let y const en lugar de var y que las variables no se pudan reasignar si no es necesario 
