26bfbf84efc9474afe03d779d58264997bc04b85
monseleon21 <121038307@upq.edu.mx>
2025-05-19 16:22:01 -0600

    B)
    Funcion no tradicional sino con Arrow

---
+++ Practica1/Convertir.js
@@ -0,0 +1,12 @@
+/* function cuadrado(numero){
+    return numero * numero;
+} */
+
+
+const cuadrado =numero => numero * numero;
+
+//console.log();
+//muestra en consola en probando con 3 numeros diferentes
+console.log(cuadrado(5));
+console.log(cuadrado(10));
+console.log(cuadrado(4));
