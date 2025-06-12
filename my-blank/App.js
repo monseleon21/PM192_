f704777d8e63fffda2479dd52185a8970f9ff908
monseleon21 <121038307@upq.edu.mx>
2025-06-11 18:32:03 -0600

    Creacion de propiedad

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,17 @@
+/* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+/* zona 3, estilos */ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
